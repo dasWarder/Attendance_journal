4374a9ab8e3bc9c5056a163513abaff7b548c6c3
dasWarder <Al.94_94@mail.ru>
2021-07-22 16:56:35 +0300

    1) Make some refactoring for controllers and services; 2) school class findAll now pageable; 3) Add documentation for some methods

--- journal-common/common/model/src/main/java/by/itechart/model/util/ValidationUtil.java
+++ journal-common/common/model/src/main/java/by/itechart/model/util/ValidationUtil.java
@@ -11,18 +11,20 @@ import java.util.Optional;
 @Slf4j
 public class ValidationUtil {
 
-
+    /**
+     * A method to validate input params for services. If Null throw NullPointerException
+     * @param params input params of a service method, that must be validated
+     */
     public static void validateParams(Object... params) {
 
         log.info("Validate input params");
 
         Arrays.stream(params).forEach(param -> {
-            if (param == null) {
 
+            if (param == null) {
                 log.error("Input parameter with type = {} is NULL", param
                                                                         .getClass()
                                                                         .getName());
-
                 throw new NullPointerException(String.format("The param of type = {} is NULL",
                                                                                               param
                                                                                                    .getClass()
@@ -31,6 +33,14 @@ public class ValidationUtil {
         });
     }
 
+    /**
+     * A method for validating optional objects.
+     * @param optionalObject
+     * @param objectClass in case of an exception, the class allows to select a correct exception type and a message
+     * @param <T> Any class, which optional must be validated
+     * @return valid object of T type, received from the optional
+     * @throws Throwable an exception of various types, if the optional is empty
+     */
     public static <T> T validateOptional(Optional<T> optionalObject, Class objectClass) throws Throwable {
 
         log.info("Validate an optional not to be equals NULL");
@@ -49,8 +59,6 @@ public class ValidationUtil {
     }
 
     private static Throwable getExceptionByClassName(String className) {
-
-
 
         switch (className) {
 
--- journal-common/service/src/main/java/by/itechart/service/attending/StudentAbsenceService.java
+++ journal-common/service/src/main/java/by/itechart/service/attending/StudentAbsenceService.java
@@ -1,13 +1,14 @@
 package by.itechart.service.attending;
 
 import by.itechart.model.Student;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface StudentAbsenceService {
 
-    List<Student> getAllByAbsenceDatesAndSchoolClassId(LocalDate absenceDate, Long classId) throws Throwable;
+    List<Student> getAllByAbsenceDatesAndSchoolClassId(LocalDate absenceDate, Long classId, Pageable pageable) throws Throwable;
 
     Student addStudentToAbsenceList(Long classId, Long studentId, LocalDate absenceDate) throws Throwable;
 
